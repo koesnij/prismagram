1da2e47f1a822f471bc11c81757f6e0fa3137953
koesnij <jinseok@moonzzinMBP.local>
2020-08-21 19:42:44 +0900

    #3.4 sendMail Function with Nodemailer & MAILGUN

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import mgTransport from 'nodemailer-mailgun-transport';
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: 'no-reply@prismagram.com',
+    to: address,
+    subject: 'Login Secret for Prismagram 🔑',
+    html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/website to login`,
+  };
+  return sendMail(email);
 };
