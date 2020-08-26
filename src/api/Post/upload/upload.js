05750f69b5de7ce8557f2f3ac8db59ad0cbf8ed4
koesnij <jinseok@moonzzinMBP.local>
2020-08-26 22:49:42 +0900

    #3.22 deletePost Resolver & DB Constraint

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -5,9 +5,10 @@ export default {
     upload: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const { caption, files } = args;
+      const { caption, location, files } = args;
       const post = await prisma.createPost({
         caption,
+        location,
         user: { connect: { id: user.id } },
       });
       files.forEach(
