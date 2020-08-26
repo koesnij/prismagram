9e07b3ac4cf06afbe53d264dd74de502d3bb8111
koesnij <jinseok@moonzzinMBP.local>
2020-08-26 19:09:38 +0900

    #3.21 editPost Resolver

---
+++ src/api/Post/editPost/editPost.js
@@ -0,0 +1,27 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+const DELETE = 'DELETE';
+const EDIT = 'EDIT';
+
+export default {
+  Mutation: {
+    editPost: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { id, caption, location, action } = args;
+      const post = await prisma.$exists.post({ id, user: { id: user.id } });
+      if (post) {
+        if (action === EDIT) {
+          return prisma.updatePost({
+            where: { id },
+            data: { caption, location },
+          });
+        } else if (action === DELETE) {
+          return prisma.deletePost({ id });
+        }
+      } else {
+        throw Error('You cant do that');
+      }
+    },
+  },
+};
