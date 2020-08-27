baf0d74b1d7ee04d4d71e71ce97345033d3a4092
koesnij <jinseok@moonzzinMBP.local>
2020-08-27 19:06:27 +0900

    #3.29 Post Fragment Refactoring

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,9 @@ import { prisma } from '../../../generated/prisma-client';
 
 export default {
   Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       return prisma.$exists.like({
@@ -15,9 +18,9 @@ export default {
         ],
       });
     },
-    likeCount: parent =>
+    likeCount: ({ id }) =>
       prisma
-        .likesConnection({ where: { post: { id: parent.id } } })
+        .likesConnection({ where: { post: { id } } })
         .aggregate()
         .count(),
   },
