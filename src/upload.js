70620abaafd173001d5e69d7a38c3a8ecfa2973a
koesnij <m_jinseok@naver.com>
2020-09-24 20:35:33 +0900

    upload fix

--- src/upload.js
+++ src/upload.js
@@ -3,7 +3,8 @@ import multer from 'multer';
 export const uploadMiddleware = multer({ dest: 'uploads/' }).single('file');
 
 export const uploadController = (req, res) => {
-  const { file } = req;
-  console.log(file);
-  res.end();
+  const {
+    file: { path },
+  } = req;
+  res.json({ path });
 };
