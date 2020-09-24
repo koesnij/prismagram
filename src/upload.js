cfbb37d997f91462cd86306d8e75baf95bb40af2
koesnij <m_jinseok@naver.com>
2020-09-24 21:48:38 +0900

    upload to AWS S3

--- src/upload.js
+++ src/upload.js
@@ -1,10 +1,31 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
-export const uploadMiddleware = multer({ dest: 'uploads/' }).single('file');
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: 'ap-northeast-2',
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'prismagram-koesnij',
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+export const uploadMiddleware = upload.single('file');
 
 export const uploadController = (req, res) => {
   const {
-    file: { path },
+    file: { location },
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
